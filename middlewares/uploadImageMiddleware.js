1e57ba56945c7a408de0fe028d684f76991a5d41
Paula <poularefaat86@gmail.com>
2024-09-20 20:12:36 +0300

    🚑 Handling upload file middleware

---
+++ middlewares/uploadImageMiddleware.js
@@ -0,0 +1,24 @@
+const multer = require("multer");
+const ApiError = require("../utils/ApiError");
+
+const multerOptions = (media) => {
+  const multerStorage = multer.memoryStorage();
+
+  const multerFilter = function (req, file, cb) {
+    const allowedMediaTypes = media.split("|");
+    if (allowedMediaTypes.some((type) => file.mimetype.startsWith(type))) {
+      cb(null, true);
+    } else {
+      cb(new ApiError(`Only ${media} Allowed`, 400), false);
+    }
+  };
+
+  const upload = multer({ storage: multerStorage, fileFilter: multerFilter });
+  return upload;
+};
+
+exports.uploadSingleMedia = (fieldName, media) =>
+  multerOptions(media).single(fieldName);
+
+exports.uploadMixOfMedia = (arrayOfFields, media) =>
+  multerOptions(media).fields(arrayOfFields);
