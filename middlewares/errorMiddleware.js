46b820b6911edf4b240b29bd055d667c40c136ad
Paula <poularefaat86@gmail.com>
2024-09-20 20:14:17 +0300

    🐛 fixing error handling to handel json response for JWT error

--- middlewares/errorMiddleware.js
+++ middlewares/errorMiddleware.js
@@ -1,38 +1,45 @@
-//GLobal error handling middleware
 const ApiError = require("../utils/ApiError");
 
-//when i pass 4 parameters express will understands that error handling middleware
 const sendErrorForDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
     error: err,
     message: err.message,
-    //where did error happpen exactly
-    stack: err.stack,
+    stack: err.stack, // for debugging
   });
 };
+
 const sendErrorForProd = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
-    message: err.message,
+    message: err.message || "Something went wrong!",
   });
 };
 
 const handleJwtInvalidSignature = () =>
-  new ApiError("Invalid token ,please login again...", 401);
+  new ApiError("Invalid token, please login again...", 401);
 
 const handleJwtExpired = () =>
-  new ApiError("Expired token ,please login again...", 401);
+  new ApiError("Expired token, please login again...", 401);
 
+// Global error handling middleware
 const globalError = (err, req, res, next) => {
+  // Set default error status and message if not set
   err.statusCode = err.statusCode || 500;
   err.status = err.status || "error";
 
+  // Check if the environment is development or production
   if (process.env.NODE_ENV === "development") {
-    sendErrorForDev(err, res);
-  } else {
+    // Handle JWT errors
     if (err.name === "JsonWebTokenError") err = handleJwtInvalidSignature();
     if (err.name === "TokenExpiredError") err = handleJwtExpired();
+    // Send error in development mode
+    sendErrorForDev(err, res);
+  } else {
+    // Handle JWT errors
+    if (err.name === "JsonWebTokenError") err = handleJwtInvalidSignature();
+    if (err.name === "TokenExpiredError") err = handleJwtExpired();
+    // Send error in production mode
     sendErrorForProd(err, res);
   }
 };
