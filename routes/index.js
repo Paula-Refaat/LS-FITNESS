fb41d8f4ba3aafc61309ca1ef1d921b3ff4ddbe8
Paula <poularefaat86@gmail.com>
2024-10-03 19:52:58 +0300

    🚧 Server updates

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,10 @@
 const multer = require("multer"); // Import multer
 const authRoute = require("./apis/authRoute");
 const userRoute = require("./apis/userRoute");
+const categoryRoute = require("./apis/categoryRoute");
+const bodyPartRoute = require("./apis/bodyPartRoute");
+const exerciseRoute = require("./apis/exerciseRoute");
+
 const mountRoute = (app) => {
   // Configure multer to handle form-data (optional storage)
   const upload = multer();
@@ -8,6 +12,9 @@ const mountRoute = (app) => {
   // Mount Routes with multer middleware for form-data
   app.use("/api/v1/auth", upload.none(), authRoute);
   app.use("/api/v1/users", userRoute);
+  app.use("/api/v1/categories", upload.none(), categoryRoute);
+  app.use("/api/v1/bodyParts", upload.none(), bodyPartRoute);
+  app.use("/api/v1/exercises", upload.none(), exerciseRoute);
 };
 
-module.exports = mountRoute
+module.exports = mountRoute;
--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const express = require("express");
 const morgan = require("morgan");
 const dotenv = require("dotenv");
 const passport = require("passport");
-const multer = require("multer"); // Import multer
 const cors = require("cors");
 
 dotenv.config({ path: "config.env" });
@@ -12,15 +11,19 @@ const dbConnection = require("./config/database");
 const ApiError = require("./utils/ApiError");
 const globalError = require("./middlewares/errorMiddleware");
 const mountRoute = require("./routes");
+const trimAll = require("./middlewares/trimMiddleware");
 
-// const authRoute = require("./routes/authRoute");
-
+// DB Connection
 dbConnection();
 
+// Express app
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(trimAll); // Use the trimAll middleware
 app.use(express.static(path.join(__dirname, "uploads")));
+
+// Enable CORS
 //enable other domains access your application
 app.use(
   cors({
@@ -30,15 +33,11 @@ app.use(
   })
 );
 
+// Development logging
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
   console.log(`mode: ${process.env.NODE_ENV}`);
 }
-// // Configure multer to handle form-data (optional storage)
-// const upload = multer();
-
-// Mount Routes with multer middleware for form-data
-// app.use("/api/v1/auth", upload.none(), authRoute);
 
 //Mount Routes
 mountRoute(app);
@@ -46,19 +45,21 @@ mountRoute(app);
 // Initialize Passport
 app.use(passport.initialize());
 
-// Handel unhandelling Routes
+// Handel unhanding Routes
 app.all("*", (req, res, next) => {
   next(new ApiError(`Can't found this Route : ${req.originalUrl}`, 400));
 });
 
-// Global error handelling middleware
+// Global error handling middleware
 app.use(globalError);
 
+// Server
 const PORT = process.env.PORT || 8000;
 const server = app.listen(PORT, () => {
   console.log(`App Running on port ${PORT}`);
 });
 
+// Handle unhandled promise rejection
 process.on("unhandledRejection", (error) => {
   console.log(`unhandledRejection Error : ${error.name} | ${error.message}`);
   server.close(() => {
