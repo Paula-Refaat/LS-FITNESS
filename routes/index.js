b2655b46bf88e6e600c122e10f94a4526dee345e
Paula-Refaat <poularefaat86@gmail.com>
2024-11-02 13:35:43 +0200

    ✨ adding nutrition functionality

--- routes/index.js
+++ routes/index.js
@@ -6,10 +6,12 @@ const bodyPartRoute = require("./apis/bodyPartRoute");
 const exerciseRoute = require("./apis/exerciseRoute");
 const mealsCalculationRoute = require("./apis/mealsCalculationRoute");
 const progressRoute = require("./apis/progressRoute");
+const { getNutritionDetails } = require("../utils/helpers/nutrition");
 
 const mountRoute = (app) => {
   // Configure multer to handle form-data (optional storage)
-  const upload = multer();
+  const storage = multer.memoryStorage();
+  const upload = multer({ storage: storage });
 
   // Mount Routes with multer middleware for form-data
   app.use("/api/v1/auth", upload.none(), authRoute);
@@ -19,6 +21,9 @@ const mountRoute = (app) => {
   app.use("/api/v1/exercises", upload.none(), exerciseRoute);
   app.use("/api/v1/mealsCalculation", upload.none(), mealsCalculationRoute);
   app.use("/api/v1/progress", upload.none(), progressRoute);
+
+  // API endpoint to upload and analyze an image
+  app.post("/api/v1/nutrition", upload.single("image"), getNutritionDetails);
 };
 
 module.exports = mountRoute;
---
+++ utils/helpers/nutrition.js
@@ -0,0 +1,41 @@
+const axios = require("axios");
+const FormData = require("form-data");
+
+exports.getNutritionDetails = async (req, res) => {
+  try {
+    const url = process.env.NUTRITION_URL;
+    const apiKey = process.env.NUTRITION_API_KEY; // Replace with your actual API key
+
+    // Create a new FormData instance and append the image
+    const form = new FormData();
+    form.append("image", req.file.buffer, {
+      filename: req.file.originalname, // Use the original filename
+      contentType: req.file.mimetype, // Set the content type
+    });
+
+    // Make the POST request to the external API
+    const response = await axios.post(url, form, {
+      headers: {
+        Authorization: `Api-Key ${apiKey}`,
+        ...form.getHeaders(), // Get headers for form-data
+      },
+    });
+
+    // Send the response back to the client
+    const nutritionalResponse = response.data.items.map((item) => {
+      return item.food.map((test) => {
+        return {
+          displayName: test.food_info.display_name,
+          nutritionData: test.food_info.nutrition,
+        };
+      });
+    });
+    res.json(nutritionalResponse);
+  } catch (error) {
+    console.error(
+      "Error:",
+      error.response ? error.response.data : error.message
+    );
+    res.status(500).send("An error occurred while analyzing the image.");
+  }
+};
