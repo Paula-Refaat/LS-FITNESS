ec4698905fade37afe1666c9208a55c93f34e2cc
Paula-Refaat <poularefaat86@gmail.com>
2024-11-07 17:20:24 +0200

    👽️ Adding nutritious analysis from external api

--- utils/helpers/nutrition.js
+++ utils/helpers/nutrition.js
@@ -2,45 +2,7 @@ const axios = require("axios");
 const FormData = require("form-data");
 const sharp = require("sharp");
 
-// exports.getNutritionDetails = async (req, res) => {
-//   try {
-//     const url = process.env.NUTRITION_URL;
-//     const apiKey = process.env.NUTRITION_API_KEY; // Replace with your actual API key
-
-//     // Create a new FormData instance and append the image
-//     const form = new FormData();
-//     form.append("image", req.file.buffer, {
-//       filename: `${req.file.originalname.split(".")[0]}.png`, // Set filename to use `.png` extension
-//       contentType: "image/png", // Set the content type to PNG
-//     });
-
-//     // Make the POST request to the external API
-//     const response = await axios.post(url, form, {
-//       headers: {
-//         Authorization: `Api-Key ${apiKey}`,
-//         ...form.getHeaders(), // Get headers for form-data
-//       },
-//     });
-
-//     // Send the response back to the client
-//     const nutritionalResponse = response.data.items.map((item) => {
-//       return item.food.map((test) => {
-//         return {
-//           displayName: test.food_info.display_name,
-//           nutritionData: test.food_info.nutrition,
-//         };
-//       });
-//     });
-//     res.json(nutritionalResponse);
-//   } catch (error) {
-//     console.error(
-//       "Error:",
-//       error.response ? error.response.data : error.message
-//     );
-//     res.status(500).send("An error occurred while analyzing the image.");
-//   }
-// };
-
+// Function to convert image to PNG format and get nutritional details using an external API
 exports.getNutritionDetails = async (req, res) => {
   try {
     const url = process.env.NUTRITION_URL;
@@ -66,19 +28,39 @@ exports.getNutritionDetails = async (req, res) => {
 
     // Send the response back to the client
     const nutritionalResponse = response.data.items.map((item) => {
-      return item.food.map((test) => {
+      return item.food.map((eat) => {
         return {
-          displayName: test.food_info.display_name,
-          nutritionData: test.food_info.nutrition,
+          displayName: eat.food_info.display_name,
+          nutritionData: eat.food_info.nutrition,
         };
       });
     });
-    res.json(nutritionalResponse);
+
+    const finalResponse = nutritionalResponse[0].map((item) => {
+      return {
+        displayName: item.displayName,
+
+        // Transform the object to the desired array format
+        nutritionData: Object.entries(item.nutritionData).map(
+          ([key, value]) => {
+            return {
+              name: key.replace("_100g", ""), // Remove '_100g' suffix if needed
+              value: value,
+            };
+          }
+        ),
+      };
+    });
+    res.json({
+      data: finalResponse,
+    });
   } catch (error) {
     console.error(
       "Error:",
       error.response ? error.response.data : error.message
     );
-    res.status(500).send("An error occurred while analyzing the image.");
+    res
+      .status(500)
+      .json({ message: "An error occurred while analyzing the image." });
   }
 };
