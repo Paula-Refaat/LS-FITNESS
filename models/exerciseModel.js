d2c052be79d75fa88c5f27d16c99b4c3beccfcc3
Paula-Refaat <poularefaat86@gmail.com>
2024-11-17 16:32:47 +0200

    ✅ exercise data

--- models/exerciseModel.js
+++ models/exerciseModel.js
@@ -6,42 +6,49 @@ const exerciseSchema = mongoose.Schema(
     title: {
       type: String,
       required: [true, "exercise title required"],
-      unique: [true, "exercise title must be unique"],
+      // unique: [true, "exercise title must be unique"],
       minlength: [3, "too short exercise  title "],
       maxlength: [32, "too long exercise title"],
-    },
-    deepAnatomy: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "DeepAnatomy",
     },
     bodyPart: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "BodyPart",
     },
+    toolOrMachine: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "ToolOrMachine",
+    },
+    Cardio: {
+      type: Boolean,
+    },
+    Warmup: {
+      type: Boolean,
+    },
+    recoveryAndStretching: {
+      type: Boolean,
+    },
+    deepAnatomy: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "DeepAnatomy",
+      },
+    ],
+
     targetGender: {
       type: String,
       enum: ["men", "women"],
       required: [true, "targetGender field is required"],
     },
-    //FIXME:fix this video schema
-    videoUrl: {
+    video: {
+      thumbnail: {},
+      public_id: {},
+      url: {},
+    },
+    Description: {
       type: String,
-      required: [true, "videoUrl field is required"],
-      unique: [true, "exercise video Url must be unique"],
+      minlength: [10, "too short Description "],
+      maxlength: [1000, "too long Description"],
     },
-    // must me like that
-    /*
-        video: {
-      public_id: {
-        type:string,
-      },
-      vide_url: {
-        type: String,
-        required: [true, "videoUrl field is required"],
-        unique: [true, "exercise video Url must be unique"],
-      }
-    },
-    */
     instructions: {
       type: String,
       minlength: [10, "too short instructions "],
@@ -54,26 +61,34 @@ const exerciseSchema = mongoose.Schema(
 exerciseSchema.pre(/^find/, function (next) {
   this.populate({ path: "deepAnatomy", select: "title" })
     .populate({ path: "bodyPart", select: "title" })
-    .sort({ title: 1 }); // Sort by title in ascending order
+    .populate({ path: "toolOrMachine", select: "title" });
+  // .sort({ title: 1 }); // Sort by title in ascending order
   // console.log(videoUrl)
   next();
 });
+exerciseSchema.methods.toJSON = function () {
+  const exercise = this.toObject();
 
-// exerciseSchema.methods.toJSON = function () {
-//   const obj = this.toObject();
+  // List all fields with default values as null if missing
+  const defaultFields = {
+    bodyPart: null,
+    toolOrMachine: null,
+    Cardio: null,
+    Warmup: null,
+    recoveryAndStretching: null,
+    deepAnatomy: [],
+    video: {
+      thumbnail: null,
+      public_id: null,
+      url: null,
+    },
+    Description: null,
+    instructions: null,
+  };
 
-//   // Create a copy of the object to safely remove videoUrl
-//   const { videoUrl, ...responseWithoutVideoUrl } = obj;
-//   const data = {
-//     ...responseWithoutVideoUrl,
-//     video: {
-//       url: videoUrl,
-//       public_id: videoUrl.split("/").pop(),
-//     },
-//   };
-
-//   return data;
-// };
+  // Merge missing fields with default values
+  return { ...defaultFields, ...exercise };
+};
 
 //2- create model
 const ExerciseModel = mongoose.model("Exercise", exerciseSchema);
--- services/exerciseServices.js
+++ services/exerciseServices.js
@@ -1,10 +1,5 @@
-// const mongoose = require("mongoose");
-// const asyncHandler = require("express-async-handler");
-// const ApiError = require("../utils/apiError");
 const Exercise = require("../models/exerciseModel");
-const ApiError = require("../utils/ApiError");
 const factory = require("./handllerFactory");
-const asyncHandler = require("express-async-handler");
 
 //@desc get list of Exercises
 //@route GET /api/v1/exercises
@@ -14,24 +9,7 @@ exports.getExercises = factory.getAll(Exercise, "Exercise");
 //@desc get specific exercise by id
 //@route GET /api/v1/exercises/:id
 //@access public
-exports.getExercise = asyncHandler(async (req, res, next) => {
-  const exercise = await Exercise.findById(req.params.id);
-  if (!exercise) {
-    return next(new ApiError("Exercise not found", 404));
-  }
-  const data = {
-    _id: exercise._id,
-    title: exercise.title,
-    targetGender: exercise.targetGender,
-    category: exercise.category,
-    bodyPart: exercise.bodyPart,
-    url: exercise.videoUrl, // Directly use videoUrl
-    public_id: exercise.videoUrl.split("/").pop(), // Extract public ID from video URL
-    // createdAt: exercise.createdAt,
-    // updatedAt: exercise.updatedAt,
-  };
-  res.status(200).json({ data: data });
-});
+exports.getExercise = factory.getOne(Exercise);
 
 //@desc create exercise
 //@route POST /api/v1/exercises
