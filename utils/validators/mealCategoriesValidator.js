b2441217c7a244f1e0699ca8d64feeae46ee3d5b
Paula-Refaat <poularefaat86@gmail.com>
2024-11-25 15:00:14 +0200

    🦺 adding mealCategories Calculations

---
+++ utils/validators/mealCategoriesValidator.js
@@ -0,0 +1,51 @@
+const { check } = require("express-validator");
+const validatorMiddleware = require("../../middlewares/validatorMiddleware");
+// const MealCategory = require("../../models/mealsCategoryModel");
+
+exports.createMealCategoryValidator = [
+  check("title_AR")
+    .notEmpty()
+    .withMessage("title_AR required")
+    .isLength({ min: 3 })
+    .withMessage("too short title_AR")
+    .isLength({ max: 100 })
+    .withMessage("too long title_AR"),
+  check("title_EN")
+    .notEmpty()
+    .withMessage("title_EN required")
+    .isLength({ min: 3 })
+    .withMessage("too short title_EN")
+    .isLength({ max: 100 })
+    .withMessage("too long title_EN"),
+  validatorMiddleware,
+];
+
+exports.updateMealCategoryValidator = [
+  check("title_AR")
+    .optional()
+    .notEmpty()
+    .withMessage("title_AR required")
+    .isLength({ min: 3 })
+    .withMessage("too short title_AR")
+    .isLength({ max: 100 })
+    .withMessage("too long title_AR"),
+  check("title_EN")
+    .optional()
+    .notEmpty()
+    .withMessage("title_EN required")
+    .isLength({ min: 3 })
+    .withMessage("too short title_EN")
+    .isLength({ max: 100 })
+    .withMessage("too long title_EN"),
+  validatorMiddleware,
+];
+
+exports.getOneMealCategoryValidator = [
+  check("id").isMongoId().withMessage("Invalid Exercise id format"),
+  validatorMiddleware,
+];
+
+exports.deleteMealCategoryValidator = [
+  check("id").isMongoId().withMessage("Invalid Exercise id format"),
+  validatorMiddleware,
+];
