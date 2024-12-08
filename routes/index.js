ee65d9728b3a93ed855fdcb5bd699f8336c3c3fd
Paula-Refaat <poularefaat86@gmail.com>
2024-12-08 16:35:46 +0200

    🚚 updating routes

--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,7 @@ const mealsCalculationRoute = require("./apis/mealsCalculationRoute");
 const progressRoute = require("./apis/progressRoute");
 const mealsCategoryRoute = require("./apis/mealsCategoryRoute");
 const couponRoute = require("./apis/couponRoute");
+const categoryRoute = require("./apis/categoryRoute");
 const courseRoute = require("./apis/courseRoute");
 const lessonRoute = require("./apis/lessonRoute");
 const quizRoute = require("./apis/quizRoute");
@@ -17,23 +18,25 @@ const orderRoute = require("./apis/orderRoute");
 const vimeoSignedUrlRoute = require("./apis/vimeoSignedUrlRoute");
 
 const { getNutritionDetails } = require("../utils/helpers/nutrition");
+const trimAll = require("../middlewares/trimMiddleware");
 
 const mountRoute = (app) => {
   // Configure multer to handle form-data (optional storage)
   const storage = multer.memoryStorage();
   const upload = multer({ storage: storage });
-
+  app.use(trimAll);
   // Mount Routes with multer middleware for form-data
-  app.use("/api/v1/auth", upload.none(), authRoute);
+  app.use("/api/v1/auth", upload.none(), trimAll, authRoute);
   app.use("/api/v1/users", userRoute);
-  app.use("/api/v1/bodyParts", upload.none(), bodyPartRoute);
-  app.use("/api/v1/deepAnatomy", upload.none(), deepAnatomyRoute);
-  app.use("/api/v1/toolOrMachine", upload.none(), toolOrMachineRoute);
-  app.use("/api/v1/exercises", upload.none(), exerciseRoute);
-  app.use("/api/v1/mealsCalculation", mealsCalculationRoute);
-  app.use("/api/v1/progress", upload.none(), progressRoute);
-  app.use("/api/v1/mealsCategory", upload.none(), mealsCategoryRoute);
-  app.use("/api/v1/coupons", upload.none(), couponRoute);
+  app.use("/api/v1/bodyParts", upload.none(), trimAll, bodyPartRoute);
+  app.use("/api/v1/deepAnatomy", upload.none(), trimAll, deepAnatomyRoute);
+  app.use("/api/v1/toolOrMachine", upload.none(), trimAll, toolOrMachineRoute);
+  app.use("/api/v1/exercises", upload.none(), trimAll, exerciseRoute);
+  app.use("/api/v1/mealsCalculation", trimAll, mealsCalculationRoute);
+  app.use("/api/v1/progress", upload.none(), trimAll, progressRoute);
+  app.use("/api/v1/mealsCategory", upload.none(), trimAll, mealsCategoryRoute);
+  app.use("/api/v1/coupons", upload.none(), trimAll, couponRoute);
+  app.use("/api/v1/categories", upload.none(), trimAll, categoryRoute);
   app.use("/api/v1/courses", courseRoute);
   app.use("/api/v1/lessons", lessonRoute);
   app.use("/api/v1/quizzes", quizRoute);
