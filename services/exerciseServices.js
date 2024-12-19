4681d4bb99f6841df149bdd01d04ff3931f88b72
Paula-Refaat <poularefaat86@gmail.com>
2024-12-19 09:22:47 +0200

    🐛 fixing response bugs

--- services/exerciseServices.js
+++ services/exerciseServices.js
@@ -44,9 +44,39 @@ exports.filterOnExercisesInTrash = (req, res, next) => {
 };
 // Function to get thumbnails using the video ID from Vimeo URL
 exports.handlingVideoResponse = async (req, res, next) => {
-  const videoResponse = await getThumbnailsFromUrl(req.body.vimeo_video_Url);
-  req.body.video = videoResponse;
-  next();
+  try {
+    // إذا كان الطلب تحديث ولم يتم إرسال vimeo_video_Url، تجاوز العملية
+    if (
+      (req.method === "PUT" || req.method === "PATCH") &&
+      !req.body.vimeo_video_Url
+    ) {
+      // console.log()
+      return next();
+    }
+
+    // استدعاء getThumbnailsFromUrl فقط إذا كان vimeo_video_Url موجودًا
+    if (req.body.vimeo_video_Url) {
+      const videoResponse = await getThumbnailsFromUrl(
+        req.body.vimeo_video_Url
+      );
+      if (videoResponse.success === false) {
+        return res.status(400).json({
+          status: "error",
+          message: "Invalid Vimeo video URL",
+        });
+      }
+
+      req.body.video = videoResponse;
+    }
+
+    next();
+  } catch (error) {
+    console.error("Error in handlingVideoResponse:", error.message);
+    res.status(500).json({
+      status: "error",
+      message: "An error occurred while processing the video response",
+    });
+  }
 };
 
 //@desc get list of Exercises
