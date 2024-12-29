312fbadfcf0b13ef3f5bd923117494061f4869ff
Paula-Refaat <poularefaat86@gmail.com>
2024-12-29 22:49:08 +0200

    🐛 fixing a bug

--- models/trainingPlanModel.js
+++ models/trainingPlanModel.js
@@ -6,21 +6,21 @@ const trainingPlanSchema = new mongoose.Schema(
     title: { type: String, required: true }, // اسم الخطة
     description: { type: String, required: true }, // الوصف
     image: { type: String, required: true }, // الصورة
-    // days: [
-    //   {
-    //     dayNumber: { type: Number, required: true }, // اليوم رقم
-    //     exercises: [
-    //       {
-    //         exercise: { type: mongoose.Schema.Types.ObjectId, ref: "Exercise" }, // التمرين
+    days: [
+      {
+        dayNumber: { type: Number, required: true }, // اليوم رقم
+        exercises: [
+          {
+            exercise: { type: mongoose.Schema.Types.ObjectId, ref: "Exercise" }, // التمرين
 
-    //         sets: { type: Number, required: true }, // عدد المجموعات
-    //         reps: { type: Number, required: true }, // عدد التكرارات
-    //         restBetweenSets: { type: Number, required: true }, // الراحة بين المجموعات (بالثواني)
-    //         restBetweenExercises: { type: Number, required: true }, // الراحة بين التمارين (بالثواني)
-    //       },
-    //     ],
-    //   },
-    // ],
+            sets: { type: Number, required: true }, // عدد المجموعات
+            reps: { type: Number, required: true }, // عدد التكرارات
+            restBetweenSets: { type: Number, required: true }, // الراحة بين المجموعات (بالثواني)
+            restBetweenExercises: { type: Number, required: true }, // الراحة بين التمارين (بالثواني)
+          },
+        ],
+      },
+    ],
 
     isDeleted: { type: Boolean, default: false },
     deletedAt: { type: Date },
