083b6067f7bda9ed878fbd24f065afd72852501b
Paula-Refaat <poularefaat86@gmail.com>
2025-01-19 19:17:41 +0200

    ✨ adding permissions to sub-admin role to perform policies functionality

--- routes/apis/policiesRoute.js
+++ routes/apis/policiesRoute.js
@@ -2,21 +2,19 @@ const express = require("express");
 
 const authServices = require("../../services/authServices");
 const {
-  createPolicy,
   getPolicies,
   getSinglePolicy,
   updatePolicy,
 } = require("../../services/policiesServices");
+const checkPermission = require("../../middlewares/permissionMiddleware");
 
 const router = express.Router();
 router.use(authServices.protect, authServices.allowTo("sub-admin", "admin"));
 
-router.post("/", createPolicy);
+router.get("/", checkPermission("Policies", "read"), getPolicies);
 
-router.get("/", getPolicies);
+router.get("/:id", checkPermission("Policies", "read"), getSinglePolicy);
 
-router.get("/:id", getSinglePolicy);
-
-router.put("/:id", updatePolicy);
+router.put("/:id", checkPermission("Policies", "update"), updatePolicy);
 
 module.exports = router;
